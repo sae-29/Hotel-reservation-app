4842b07a2c87cf2149216fdcbf0363c938752c91
saee Bandal <saeebandal@gmail.com>
2026-03-26 13:27:57 +0530

    App for production deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template
 from models import HotelModel
 from services import BookingService
 import random
+import os
 
 app = Flask(__name__)
 
@@ -79,4 +80,4 @@ def reset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
